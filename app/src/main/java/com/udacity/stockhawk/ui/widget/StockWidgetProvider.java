d7ab128d481fcdb1dc1727a4b70b7515113335f5
agomez40 <lagomez40@gmail.com>
2017-05-15 19:36:59 -0500

    chore(): start work on StockHawk widget

---
+++ app/src/main/java/com/udacity/stockhawk/ui/widget/StockWidgetProvider.java
@@ -0,0 +1,12 @@
+package com.udacity.stockhawk.ui.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class StockWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+}
